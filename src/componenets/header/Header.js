bc268712fd96215e999212aa652cc818b14b1e90
IronHarambe <49575350+IronHarambe@users.noreply.github.com>
2020-09-28 12:49:51 +1000

    Deafult Body

---
+++ src/componenets/header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>The Header</h1>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/componenets/home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Home.scss";
+
+function Home() {
+  return (
+    <div className="home">
+      <img src="/images/ill.jpg" alt="" />
+    </div>
+  );
+}
+
+export default Home;
