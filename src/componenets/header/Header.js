3782e99051e69e75ca07fbb572b1d9ae95076ea9
IronHarambe <49575350+IronHarambe@users.noreply.github.com>
2020-10-12 18:22:42 +1100

    fix

--- src/componenets/header/Header.js
+++ src/componenets/header/Header.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { Navbar, Nav, NavDropdown } from "react-bootstrap";
-import { NavLink, Route, Link } from "react-router-dom";
+import { Navbar, Nav } from "react-bootstrap";
 
 function Header() {
   return (
