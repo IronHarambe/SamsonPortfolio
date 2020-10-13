20b5878fda8c64fbae36553e0a7b174ae3101267
IronHarambe <49575350+IronHarambe@users.noreply.github.com>
2020-10-13 19:10:05 +1100

    initial setup for project mobile view

--- src/componenets/project/Project.js
+++ src/componenets/project/Project.js
@@ -9,7 +9,7 @@ function Project() {
         <div className="intro">
           <h1>My Projects!</h1>
           <p>
-            Check out my projects! My projects are my attempt at creating
+            Check out my projects! The projects are my attempt at creating
             websites that can solve real world problems.
           </p>
         </div>
@@ -17,8 +17,20 @@ function Project() {
           <Card
             title="JTravels"
             description="Brochure type websites for travelling to Japan!"
-            image=""
+            image="/images/projects/jtravel.PNG"
           ></Card>
+            <Card
+            title="ART NATION"
+            description="Shopping e-commerse website for purchase of painting. Has add cart and checkout functions."
+            image="/images/projects/home2.PNG"
+          ></Card>
+            <Card
+            title="Old Portfolio"
+            description="First portfolio website using html, css and js. "
+            image="/images/projects/old.PNG"
+          ></Card>
+       
+          
         </div>
       </div>
     </div>
