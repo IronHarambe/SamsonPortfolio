46d0668345e76a7961ac7d84d35763fac0a79c46
IronHarambe <49575350+IronHarambe@users.noreply.github.com>
2020-10-14 19:39:52 +1100

    skill section added for mobile view

--- src/componenets/skill/Skills.js
+++ src/componenets/skill/Skills.js
@@ -7,9 +7,27 @@ function Skills() {
     <div className="skills">
       <h1>Check out my Skills!</h1>
       <div className="skill__wrapper">
-        <Skill title="HTML" image="/images/ill.jpg"></Skill>
-        <Skill title="CSS" image="/images/ill.jpg"></Skill>
-        <Skill title="JS" image="/images/ill.jpg"></Skill>
+        <h2>Front-End</h2>
+        <div className="frontend">
+          <Skill title="HTML" image="/images/skill/html.png"></Skill>
+          <Skill title="CSS" image="/images/skill/css.png"></Skill>
+          <Skill title="Javascript" image="/images/skill/js.png"></Skill>
+          <Skill title="React" image="/images/skill/react.png"></Skill>
+          <Skill title="Sass" image="/images/skill/sass.png"></Skill>
+          <Skill title="Bootstrap" image="/images/skill/bootstrap.png"></Skill>
+          <Skill title="Node" image="/images/skill/node.png"></Skill>
+          <Skill title="Git" image="/images/skill/git.png"></Skill>
+        </div>
+
+        <h2>Other</h2>
+        <div className="other">
+          <Skill title="Adobe XD" image="/images/skill/other/adobe.jpg"></Skill>
+          <Skill
+            title="Firebase"
+            image="/images/skill/other/firebase.png"
+          ></Skill>
+          <Skill title="Unity" image="/images/skill/other/unity.jpg"></Skill>
+        </div>
       </div>
     </div>
   );
